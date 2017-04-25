cb4a8d3aac82eea18d9d4f5acbc4e5941614130f
Yeongjin Gwak <jinjin299@gmail.com>
2017-04-25 10:47:01 +0900

    Solve Problem 7 (a) (b)

--- prob.py
+++ prob.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+from numpy import cos,pi
+
+
 class Vec:
     def __init__(self, x,y,z):
         self.x=x
@@ -14,6 +17,9 @@ class Vec:
         return Vec(self.x - other.x, self.y - other.y, self.z - other.z)
     def __mul__(self, lam):
         return Vec(lam*self.x, lam*self.y, lam*self.z)
+
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y + self.z * other.z
     
     def n(self):
         return self.n
@@ -26,15 +32,37 @@ a = []
 for i in range(12):
     a.append([])
 
-for n in range(-7,8):
-    for m in range(-7,8):
-        for l in range(-7,8):
+for n in range(2,-3,-1):
+    for m in range(2,-3,-1):
+        for l in range(2,-3,-1):
             tmp = a1 * n + a2 * m + a3 * l
             if tmp.n<12:
                 a[tmp.n].append(tmp)
 
+# Problem 7 (a)
+print "Problem 7 (a)"
+print "G2(num):"
 for i in range(12):
     print "%d(%d):" % (i, len(a[i])),
     for j in a[i]:
         print j,
     print " "
+
+# Problem 7 (b)
+for i in range(3):
+    print ""
+
+print "Problem 7 (b)"
+
+print "G2(num):"
+for i in range(12):
+    print "%d(%d):" % (i, len(a[i])),
+    for j in a[i]:
+        print cos(pi/4 * (j.dot(Vec(1,1,1)))),
+        #print j,
+    print " "
+
+for i in range(3):
+    print ""
+print "0.707106781187 menas sqrt(2)/2"
+print "The small number means 0"
